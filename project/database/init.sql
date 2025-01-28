b46c246d5026df83a8d412da00c88ab88ee5404b
themysterysolver <arjunprabhakar1910@gmail.com>
2025-01-28 12:53:14 +0530

    TEST:changes

--- project/database/init.sql
+++ project/database/init.sql
@@ -3,3 +3,4 @@ INSERT INTO vulnerabilities (target, type, cvss, exploitability, asset_value, ri
 VALUES 
 ('127.0.0.1', 'network', 9.8, 8.5, 7.2, 8.8),
 ('192.168.0.1', 'web', 7.5, 6.8, 7.0, 7.4);
+-- From prabha!
